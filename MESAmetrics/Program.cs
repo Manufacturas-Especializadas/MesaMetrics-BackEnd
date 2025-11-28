0e3598ff434cf17d8aed357b9b7b2d2be1422399
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2025-11-28 08:21:22 -0600

    Url de produccion y desarrollo

--- MESAmetrics/Program.cs
+++ MESAmetrics/Program.cs
@@ -13,12 +13,25 @@ builder.Services.AddDbContext<AppDbContext>(option =>
     option.UseSqlServer(connection);
 });
 
+var allowedConnection = builder.Configuration.GetValue<string>("OrigenesPermitidos")!.Split(',');
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigins", policy =>
+    {
+        policy.WithOrigins(allowedConnection)
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowSpecificOrigins");
 
 app.UseAuthorization();
 
