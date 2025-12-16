38fbe5043725c6f8227917831ceef06e43d278c2
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2025-12-16 07:24:14 -0600

    Cambios del branch RealTime

---
+++ MESAmetrics/Dtos/MachineMetricsDto.cs
@@ -0,0 +1,21 @@
+﻿namespace MESAmetrics.Dtos
+{
+    public class MachineMetricsDto
+    {
+        public string MachineName { get; set; }
+
+        public string Shift { get; set; }
+
+        public string Status { get; set; }
+
+        public string StatusDuration { get; set; }
+
+        public string Availability { get; set; }
+
+        public string ProductionTime { get; set; }
+
+        public string StopTime { get; set; }
+
+        public string Stops { get; set; }
+    }
+}
---
+++ MESAmetrics/Hubs/MachineHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace MESAmetrics.Hubs
+{
+    public class MachineHub : Hub
+    {
+        public async Task JoinGroup(string realTimeId)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, realTimeId);
+        }
+    }
+}
